55cf8c9890c5395add07561ee5289cc224e2f9bb
darlwen <wenjuandou@gmail.com>
2022-04-11 16:31:14 +0800

    add debug log

--- src/Viewer.cc
+++ src/Viewer.cc
@@ -335,6 +335,7 @@ void Viewer::Run()
             cv::resize(toShow, toShow, cv::Size(width, height));
         }
 
+        cout << "toShow value: " << toShow << endl;
         cv::imshow("ORB-SLAM3: Current Frame",toShow);
         cv::waitKey(mT);
 
