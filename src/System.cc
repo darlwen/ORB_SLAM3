36148f8746dcca18d13f1c28480cf10f94dce709
darlwen <wenjuandou@gmail.com>
2022-04-22 21:36:55 +0800

    add debug log

--- src/System.cc
+++ src/System.cc
@@ -1495,6 +1495,7 @@ void System::SaveAtlas(int type){
 
         // Save the current session
         mpAtlas->PreSave();
+        cout << "Finish saving atlas map info" << endl;
 
         string pathSaveFileName = "./";
         string tempPathSaveFileName = pathSaveFileName.append(mStrSaveAtlasToFile);
